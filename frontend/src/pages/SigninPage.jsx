422f67a98e2db21e4b5f6d3efb69d49f1a371fe8
Priyansu Singh <priyansusingh3344@gmail.com>
2024-10-17 13:45:40 +0530

    init

--- frontend/src/pages/SigninPage.jsx
+++ frontend/src/pages/SigninPage.jsx
@@ -27,7 +27,7 @@ export default function Signin(){
         }} placeholder="123456" label={"Password"} />
         <div className="pt-4">
           <Button onClick={async () => {
-            const response = await axios.post("https://payments-application-1.onrender.com/signin", {
+            const response = await axios.post("https://payments-application-1.onrender.com/user/signin", {
                 username,
                 password
             });
